5dae24ed206f0e1226dc1d91e3b971560cccf039
js <7990434@qq.com>
2021-04-23 21:28:47 +0800

    chore: 重建视图

--- zovye/migrate/20210423001.php
+++ zovye/migrate/20210423001.php
@@ -5,10 +5,6 @@ $tb_name = 'zovye_vms';
 
 if (!We7::pdo_fieldexists($tb_name . '_device', 's1')) {
     $sql = <<<SQL
-ALTER TABLE `ims_zovye_vms_device` ADD `s3` TINYINT(1) NOT NULL DEFAULT '0' AFTER `shadow_id`, ADD INDEX (`s3`);
-ALTER TABLE `ims_zovye_vms_device` ADD `s2` TINYINT(1) NOT NULL DEFAULT '0' AFTER `shadow_id`, ADD INDEX (`s2`);
-ALTER TABLE `ims_zovye_vms_device` ADD `s1` TINYINT(1) NOT NULL DEFAULT '0' AFTER `shadow_id`, ADD INDEX (`s1`);
-
 CREATE OR REPLACE VIEW `ims_zovye_vms_agent_vw` AS
 SELECT u.*,p.name as `name`,p.createtime as `updatetime`,
 (SELECT count(*) FROM `ims_zovye_vms_device` WHERE agent_id=u.id) AS device_total

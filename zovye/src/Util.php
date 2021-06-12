91c7b32b0e55af37433d6c73b6585cd8c8127cea
js <7990434@qq.com>
2021-06-12 20:22:48 +0800

    chore: 记录测试人员

--- zovye/src/Util.php
+++ zovye/src/Util.php
@@ -1400,7 +1400,7 @@ HTML_CONTENT;
 
             //如果是营运人员测试，则不减少库存
             if (empty($params['keeper'])) {
-                $res = $device->resetPayload([$lane => -1], '设备测试');
+                $res = $device->resetPayload([$lane => -1], "设备测试：{$data['userid']}");
                 if (is_error($res)) {
                     return error(State::ERROR, '保存库存失败！');
                 }

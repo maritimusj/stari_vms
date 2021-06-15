8efa976cfe334f444194cee2a3f5af9ce244001d
js <7990434@qq.com>
2021-06-15 20:20:30 +0800

    chore: 使用事务创建代理商

--- inc/mobile/mobile.inc.php
+++ inc/mobile/mobile.inc.php
@@ -6,6 +6,8 @@
 
 namespace zovye;
 
+use RuntimeException;
+
 defined('IN_IA') or exit('Access Denied');
 
 //用户参数
@@ -28,112 +30,123 @@ $op = request::op('default');
 
 if ($op == 'save') {
 
-    $mobile = request::str('mobile');
-    if (empty($mobile) || !preg_match(REGULAR_MOBILE, $mobile)) {
-        JSON::fail('请输入正确的手机号码！');
-    }
+    $result = Util::transactionDo(function () use ($user) {
+        $mobile = request::str('mobile');
+        if (empty($mobile) || !preg_match(REGULAR_MOBILE, $mobile)) {
+            throw new RuntimeException('请输入正确的手机号码！');
+        }
 
-    if ($user->isAgent()) {
-        JSON::fail('您已经是我们的代理商！');
-    }
+        if ($user->isAgent()) {
+            throw new RuntimeException('您已经是我们的代理商！');
+        }
 
-    if ($user->isPartner()) {
-        JSON::fail('您已经是我们代理商的合伙人！');
-    }
+        if ($user->isPartner()) {
+            throw new RuntimeException('您已经是我们代理商的合伙人！');
+        }
 
-    if ($user->isKeeper()) {
-        JSON::fail('您已经是我们代理商的运营人员！');
-    }
+        if ($user->isKeeper()) {
+            throw new RuntimeException('您已经是我们代理商的运营人员！');
+        }
 
-    $res = User::findOne(['id <>' => $user->getId(), 'mobile' => $mobile]);
-    if (!empty($res)) {
-        JSON::fail('手机号码已经被其它用户使用！');
-    }
+        $res = User::findOne(['id <>' => $user->getId(), 'mobile' => $mobile]);
+        if (!empty($res)) {
+            throw new RuntimeException('手机号码已经被其它用户使用！');
+        }
 
-    $user->setMobile($mobile);
-    if (!$user->save()) {
-        JSON::fail('保存手机号码失败！');
-    }
+        $user->setMobile($mobile);
+        if (!$user->save()) {
+            throw new RuntimeException('保存手机号码失败！');
+        }
 
-    if (App::agentRegMode() == Agent::REG_MODE_AUTO) {
-        $level = App::agentDefaultLevel();
-        $agent_data = [
-            'name' => $user->getName(),
-            'company' => '<未登记>',
-            'license' => '',
-            'level' => $level,
-            'area' => [],
-            'notice' => [
-                'agentApp' => 1,
-                'remainWarning' => 1,
-                'deviceError' => 1,
-                'reviewResult' => 1,
-                'agentMsg' => 1,
-                'deviceOnline' => 1,
-            ],
-            'funcs' => App::agentDefaultFuncs(),
-            'superior' => [],
-            'location' => [
-                'validate' => [
-                    'enabled' => App::isLocationValidateEnabled() ? 1 : 0,
-                    'distance' => App::userLocationValidateDistance(),
+        if (App::agentRegMode() == Agent::REG_MODE_AUTO) {
+            $level = App::agentDefaultLevel();
+            $agent_data = [
+                'name' => $user->getName(),
+                'company' => '<未登记>',
+                'license' => '',
+                'level' => $level,
+                'area' => [],
+                'notice' => [
+                    'agentApp' => 1,
+                    'remainWarning' => 1,
+                    'deviceError' => 1,
+                    'reviewResult' => 1,
+                    'agentMsg' => 1,
+                    'deviceOnline' => 1,
                 ],
-            ],
-        ];
-
-        if (App::isCommissionEnabled()) {
-
-            $agent_data['commission'] = [
-                'enabled' => 1,
-                'fee_type' => App::agentDefaultCommissionFeeType(),
-                'fee' => App::agentDefaultCommissionFee(),
+                'funcs' => App::agentDefaultFuncs(),
+                'superior' => [],
+                'location' => [
+                    'validate' => [
+                        'enabled' => App::isLocationValidateEnabled() ? 1 : 0,
+                        'distance' => App::userLocationValidateDistance(),
+                    ],
+                ],
             ];
 
-            //佣金分享
-            if (App::isAgentGSPEnabled()) {
-                $agent_data['gsp'] = [
+            if (App::isCommissionEnabled()) {
+
+                $agent_data['commission'] = [
                     'enabled' => 1,
-                    'mode' => 'rel',
-                    'rel' => App::agentDefaultGSP(),
-                    'mode_type' => App::agentDefaultGSDModeType(),
+                    'fee_type' => App::agentDefaultCommissionFeeType(),
+                    'fee' => App::agentDefaultCommissionFee(),
                 ];
+
+                //佣金分享
+                if (App::isAgentGSPEnabled()) {
+                    $agent_data['gsp'] = [
+                        'enabled' => 1,
+                        'mode' => 'rel',
+                        'rel' => App::agentDefaultGSP(),
+                        'mode_type' => App::agentDefaultGSDModeType(),
+                    ];
+                }
+
+                //佣金奖励
+                if (App::isAgentBonusEnabled()) {
+                    $agent_data['bonus'] = App::agentDefaultBonus();
+                }
             }
 
-            //佣金奖励
-            if (App::isAgentBonusEnabled()) {
-                $agent_data['bonus'] = App::agentDefaultBonus();
+            $code = request::str('code');
+            if (App::isAgentReferralEnabled()) {
+                if (empty($code)) {
+                    throw new RuntimeException('请填写推荐码！');
+                }
+            }
+            if (!empty($code)) {
+                $superior = Referral::getAgent($code);
+                if (empty($superior)) {
+                    throw new RuntimeException('推荐码不正确！');
+                }
+                $agent_data['superior'] = [
+                    'openid' => $superior->getOpenid(),
+                    'name' => $superior->getName(),
+                ];
+                $user->setSuperiorId($superior->getId());
+            }
+
+            if (!$user->updateSettings('agentData', $agent_data)) {
+                throw new RuntimeException('失败01，请联系管理员！');
+            }
+            if (!$user->setAgent($level)) {
+                throw new RuntimeException('失败02，请联系管理员！');
+            }
+
+            if ($user->save()) {
+                //使用控制中心推送通知
+                Job::newAgent($user->getId());
+                return ['message' => '恭喜您已经成功注册为代理商！'];
             }
         }
+        return ['message' => '手机号码保存成功,请联系客服为您授权！'];
+    });
 
-        $code = request::str('code');
-        if (App::isAgentReferralEnabled()) {
-            if (empty($code)) {
-                JSON::fail('请填写推荐码！');
-            }
-        }
-        if (!empty($code)) {
-            $superior = Referral::getAgent($code);
-            if (empty($superior)) {
-                JSON::fail('推荐码不正确！');
-            }
-            $agent_data['superior'] = [
-                'openid' => $superior->getOpenid(),
-                'name' => $superior->getName(),
-            ];
-            $user->setSuperiorId($superior->getId());
-        }
-
-        $user->updateSettings('agentData', $agent_data);
-        $user->setAgent($level);
-
-        if ($user->save()) {
-            //使用控制中心推送通知
-            Job::newAgent($user->getId());
-            JSON::success('恭喜您已经成功注册为代理商！');
-        }
+    if (is_error($result)) {
+        JSON::fail($result['message']);
     }
 
-    JSON::success('手机号码保存成功,请联系客服为您授权！');
+    JSON::success(empty($result['message']) ? '失败！' : $result['message']);
 
 } else {
 

7ce6ea54e8bfe9211fce17b50fd397d57f6f24cd
js <7990434@qq.com>
2021-07-05 08:17:39 +0800

    chore: *

--- zovye/src/DeviceEventProcessor.php
+++ zovye/src/DeviceEventProcessor.php
@@ -275,25 +275,22 @@ class DeviceEventProcessor
      */
     public static function handle(string $event, array $data)
     {
-        $ok = false;
-
         $e = self::$events[$event];
         if (isset($e)) {
             self::log($e, $data);
             $fn = $e['handler'];
-            if (is_callable($fn)) {
-                call_user_func($fn, $data);
-                $ok = true;
-            } else {
-                Util::logToFile('events', [
-                    'event' => $event,
-                    'data' => $data,
-                    'error' => 'handler is not function!',
-                ]);
+            if (!empty($fn)) {
+                if (is_callable($fn)) {
+                    call_user_func($fn, $data);
+                } else {
+                    Util::logToFile('events', [
+                        'event' => $event,
+                        'data' => $data,
+                        'error' => 'handler is not function!',
+                    ]);
+                }
             }
-        }
-
-        if (!$ok) {
+        } else {
             Util::logToFile('events', [
                 'event' => $event,
                 'data' => $data,
@@ -342,7 +339,6 @@ class DeviceEventProcessor
                     'data' => $data,
                 ]);
             }
-
         }
     }
 
@@ -426,7 +422,6 @@ class DeviceEventProcessor
      */
     public static function onAppAdvMsg(array $data)
     {
-
     }
 
     /**
@@ -530,7 +525,6 @@ class DeviceEventProcessor
             $result['mcbUID'] = strval($device->getImei());
 
             $device->appNotify('config', $result);
-
         } else {
             $result = [
                 'volume' => 10, //音量百分比 0 - 100
@@ -661,7 +655,7 @@ class DeviceEventProcessor
                 $device->setSig($data['extra']['RSSI']);
             }
 
-            if (isset($data['extra']['LAC'])) {                
+            if (isset($data['extra']['LAC'])) {
                 if (settings('device.lac.enabled')) {
                     $lastLAC = $device->settings('extra.v1.lac.v', '');
                     if (!empty($lastLAC) && $lastLAC != $data['extra']['LAC'] && empty($device->getS1())) {
@@ -669,7 +663,7 @@ class DeviceEventProcessor
                         $device->UpdateSettings('extra.v1.lac.time', time());
                     }
                 }
-                
+
                 $device->updateSettings('extra.v1.lac.v', strval($data['extra']['LAC']));
             }
 
@@ -722,7 +716,6 @@ class DeviceEventProcessor
      */
     public static function onMcbReset(array $data = [])
     {
-
     }
 
 

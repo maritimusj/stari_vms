cac4a7f4fe4423df2cc280ccb821ecd7053f605b
js <7990434@qq.com>
2021-07-28 11:00:29 +0800

    feat: package&package goods

--- zovye/src/Package.php
+++ zovye/src/Package.php
@@ -1,7 +1,44 @@
 <?php
+
 namespace zovye;
+
+use zovye\model\packageModelObj;
 
 class Package
 {
-    
+    /**
+     * @param array $data
+     * @return packageModelObj|null
+     */
+    public static function create($data = []): ?packageModelObj
+    {
+        return m('package')->create($data);
+    }
+
+    /**
+     * @param array $condition
+     * @return base\modelObjFinder
+     */
+    public static function query($condition = []): base\modelObjFinder
+    {
+        return m('package')->query($condition);
+    }
+
+    /**
+     * @param $id
+     * @return packageModelObj|null
+     */
+    public static function get($id): ?packageModelObj
+    {
+        return self::findOne(['id' => $id]);
+    }
+
+    /**
+     * @param array $condition
+     * @return packageModelObj|null
+     */
+    public static function findOne($condition = []): ?packageModelObj
+    {
+        return self::query($condition)->findOne();
+    }
 }
--- zovye/src/PackageGoods.php
+++ zovye/src/PackageGoods.php
@@ -1,7 +1,45 @@
 <?php
+
 namespace zovye;
+
+use zovye\model\package_goodsModelObj;
 
 class PackageGoods
 {
-    
+    /**
+     * @param array $data
+     * @return package_goodsModelObj|null
+     */
+    public static function create($data = []): ?package_goodsModelObj
+    {
+        return m('package_goods')->create($data);
+    }
+
+    /**
+     * @param array $condition
+     * @return base\modelObjFinder
+     */
+    public static function query($condition = []): base\modelObjFinder
+    {
+        return m('package_goods')->query($condition);
+    }
+
+    /**
+     * @param $id
+     * @return package_goodsModelObj|null
+     */
+    public static function get($id): ?package_goodsModelObj
+    {
+        return self::findOne(['id' => $id]);
+    }
+
+    /**
+     * @param array $condition
+     * @return package_goodsModelObj|null
+     */
+    public static function findOne($condition = []): ?package_goodsModelObj
+    {
+        return self::query($condition)->findOne();
+    }
+
 }

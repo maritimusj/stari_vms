498cddd5bc532879832ae2829546ddc748b10859
jjs <7990434@qq.com>
2021-11-03 13:04:13 +0800

    feat: Statistics

--- zovye/src/Statistics.php
+++ zovye/src/Statistics.php
@@ -6,11 +6,13 @@ use DateTime;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Exception;
+use zovye\model\agentModelObj;
 use zovye\model\deviceModelObj;
+use zovye\model\userModelObj;
 
 class Statistics
 {
-    public static function deviceOrderMonth(deviceModelObj $device, $month = '')
+    private static function parseMonth($month): ?DateTime
     {
         $date = null;
         try {
@@ -22,14 +24,23 @@ class Statistics
                 $date = DateTime::createFromFormat('Y-m-d', $month->format('Y-m-d'));
             }
         } catch (Exception $e) {
+            return null;
+        }
+        $date->modify('first day of this month 00:00');
+        return $date;
+    }
+
+    public static function deviceOrderMonth(deviceModelObj $device, $month = '')
+    {
+        $date = self::parseMonth($month);
+        if (!$date) {
             return [];
         }
-        
-        $date->modify('first day of this month 00:00');
+
         $begin = DateTimeImmutable::createFromMutable($date);
 
-        return Util::cachedCall($begin->format('Y-m') == date('Y-m') ? 10 : 0, function () use ($begin, $device) {
-            $end = $begin->modify('+1 month');
+        return Util::cachedCall($begin->format('Y-m') === date('Y-m') ? 10 : 0, function () use ($begin, $device) {
+            $end = $begin->modify('first day of next month 00:00');
 
             $result = [
                 'free' => 0,
@@ -70,5 +81,75 @@ class Statistics
 
             return $result;
         }, $device->getId(), $begin->format('Y-m'));
+    }
+
+    public static function userOrderMonth(userModelObj $user, $month = '', $detail = false)
+    {
+        $fn = function (DateTimeInterface $begin, DateTimeInterface $end) use ($user) {
+            $result = [
+                'order' => [
+                    'free' => 0,
+                    'fee' => 0,
+                ],
+                'commission' => [
+                    'total' => 0,
+                ]
+            ];
+
+            $result['order']['free'] = (int)Order::query()->where([
+                'agent_id' => $user->getId(),
+                'price' => 0,
+                'balance' => 0,
+                'createtime >=' => $begin->getTimestamp(),
+                'createtime <' => $end->getTimestamp()
+            ])->get('sum(num)');
+
+            $result['order']['fee'] = (int)Order::query()->where([
+                'agent_id' => $user->getId(),
+                'price >' => 0,
+                'createtime >=' => $begin->getTimestamp(),
+                'createtime <' => $end->getTimestamp(),
+            ])->get('sum(num)');
+
+            $result['commission']['total'] = (int)CommissionBalance::query()->where([
+                'openid' => $user->getOpenid(),
+                'src' => [
+                    CommissionBalance::ORDER_FREE,
+                    CommissionBalance::ORDER_BALANCE,
+                    CommissionBalance::ORDER_WX_PAY,
+                    CommissionBalance::ORDER_REFUND,
+                    CommissionBalance::GSP,
+                    CommissionBalance::BONUS,
+                ],
+                'createtime <=' => $begin->getTimestamp(),
+                'createtime >' => $end->getTimestamp(),
+            ])->get('sum(x_val)');
+
+
+            return $result;
+        };
+
+        $begin = self::parseMonth($month);
+        if (!$begin) {
+            return [];
+        }
+
+        return Util::cachedCall($begin->format('Y-m') === date('Y-m') ? 10 : 0, function () use ($fn, $begin, $detail) {
+            $end = DateTimeImmutable::createFromMutable($begin)->modify('first day of next month 00:00');
+            $result = [
+                'brief' => $fn($begin, $end),
+            ];
+
+            if ($detail) {
+                $result['list'] = [];
+                while ($begin < $end) {
+                    $start = DateTimeImmutable::createFromMutable($begin);
+                    $begin->modify('next day 00:00');
+                    $result['list'][$start->format('Y-m-d')] = $fn($start, $begin);
+                }
+            }
+
+            return $result;
+        });
     }
 }

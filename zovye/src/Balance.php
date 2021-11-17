dc807f1efeb2d8858f743d2419896746908b0fff
jjs <7990434@qq.com>
2021-11-17 09:48:29 +0800

    feat: 用户于积分的Balance

---
+++ zovye/src/Balance.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace zovye;
+
+use zovye\model\balanceModelObj;
+use zovye\model\commission_balanceModelObj;
+use zovye\model\userModelObj;
+
+class Balance
+{
+    const CACHE_EXPIRATION = 60 * 60;
+
+    private $user;
+
+    public function __construct(userModelObj $user)
+    {
+        $this->user = $user;
+    }
+
+    public function __toString(): string
+    {
+        return strval($this->total());
+    }
+
+    public static function query($condition = []): base\modelObjFinder
+    {
+        return m('balance')->where(We7::uniacid([]))->where($condition);
+    }
+
+    public static function findOne($condition = []): ?commission_balanceModelObj
+    {
+        return self::query($condition)->findOne();
+    }
+
+    /**
+     * 余额变动操作
+     * @param int $val
+     * @param int $src
+     * @param array $extra
+     * @return balanceModelObj
+     */
+    public function change(int $val, int $src, array $extra = []): ?balanceModelObj
+    {
+        if ($this->user && $val != 0) {
+            return m('balance')->create(
+                We7::uniacid(
+                    [
+                        'openid' => $this->user->getOpenid(),
+                        'src' => $src,
+                        'x_val' => $val,
+                        'extra' => json_encode($extra),
+                    ]
+                )
+            );
+        }
+
+        return null;
+    }
+
+    /**
+     * 获取当前余额
+     * @return int
+     */
+    public function total(): int
+    {
+        $total = 0;
+
+        if ($this->user) {
+
+            $openid = $this->user->getOpenid();
+            $query = Balance::query(['openid' => $openid]);
+
+            $last_id = 0;
+            $last_total = 0;
+            $last_time = 0;
+
+            $cache = $this->user->get('balance:cache', []);
+            if ($cache && isset($cache['id']) && isset($cache['total'])) {
+
+                $last_id = intval($cache['id']);
+                $last_total = intval($cache['total']);
+                $last_time = intval($cache['time']);
+
+                $query->where(['id >' => $last_id]);
+            }
+
+            if (time() - $last_time > self::CACHE_EXPIRATION) {
+
+                list($total, $id) = $query->get(['sum(x_val)', 'max(id)']);
+
+                if (isset($id) && $id > $last_id) {
+                    $total += $last_total;
+                    $locker = $this->user->acquireLocker(User::BALANCE_LOCKER);
+                    if ($locker) {
+                        $this->user->set('balance:cache', [
+                            'id' => $id,
+                            'total' => $total,
+                            'time' => time(),
+                        ]);
+                        $locker->unlock();
+                    }
+                } else {
+                    $total = $last_total;
+                }
+
+            } else {
+                $total = $query->get('sum(x_val)') + $last_total;
+            }
+        }
+
+        return $total;
+    }
+}

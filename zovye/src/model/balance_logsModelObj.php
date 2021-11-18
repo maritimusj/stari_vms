3015b30cee232a16d3ec693182037df146ee6052
js <7990434@qq.com>
2021-11-18 13:47:49 +0800

    chore: balance_logsModelObj

---
+++ zovye/src/model/balance_logsModelObj.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+namespace zovye\model;
+
+use zovye\base\modelObj;
+use function zovye\tb;
+
+class balance_logsModelObj extends modelObj
+{
+    public static function getTableName($readOrWrite): string
+    {
+        return tb('balance_logs');
+    }
+
+    /** @var int */
+    protected $id;
+
+    /** @var int */
+	protected $user_id;
+
+    /** @var int */
+	protected $account_id;
+
+	protected $extra;
+
+    /** @var int */
+	protected $createtime;
+
+	use \zovye\traits\ExtraDataGettersAndSetters;
+}

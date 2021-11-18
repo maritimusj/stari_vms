3ebec20e575465a014e070ed2726a1d13b5becb9
jjs <7990434@qq.com>
2021-11-18 09:50:17 +0800

    chore: *

--- zovye/jobs/device_err.php
+++ zovye/jobs/device_err.php
@@ -31,9 +31,9 @@ if ($op == 'device_err' && CtrlServ::checkJobSign($data)) {
     /** @var deviceModelObj $device */
     $device = Device::get(request::int('id'));
     if ($device) {
-
+        $error_msg = request::str('message');
         //设置设备错误码
-        $device->setError(request::int('errno'), request::str('message'));
+        $device->setError(request::int('errno'), $error_msg);
         $device->save();
 
         $tpl_id = settings('notice.deviceerr_tplid');
@@ -43,7 +43,7 @@ if ($op == 'device_err' && CtrlServ::checkJobSign($data)) {
                 'keyword1' => ['value' => $device->getImei()],
                 'keyword2' => ['value' => $device->getName()],
                 'keyword3' => ['value' => date('Y-m-d H:i:s')],
-                'keyword4' => ['value' => '设备出货失败：' . request::str('message')],
+                'keyword4' => ['value' => '设备出货失败：' . $error_msg],
             ];
             //通过微信模板消息给代理商推送消息
             $agent = $device->getAgent();

b339e36d8164bd83fd5f6a9e319cc457c879ac0e
jjs <7990434@qq.com>
2021-12-03 14:35:27 +0800

    chore: *

--- zovye/migrate/20211109001.php
+++ zovye/migrate/20211109001.php
@@ -12,12 +12,60 @@ SQL;
 
     $query = Account::query();
 
-    foreach($query->findAll() as $entry) {
+    foreach ($query->findAll() as $entry) {
         $type = intval($entry->settings('config.type', 0));
         $entry->setType($type);
         $entry->setState($entry->getState() != 0 ? Account::NORMAL : Account::BANNED);
         $entry->save();
     }
 
-   updateSettings('accounts.lastupdate', time());
+    $spec = [
+        Account::JFB => [
+            Util::murl('jfb'),
+            Account::JFB_NAME,
+        ],
+        Account::MOSCALE => [
+            Util::murl('moscale'),
+            Account::    MOSCALE_NAME,
+        ],
+        Account::YUNFENBA => [
+            Util::murl('yunfenba'),
+            Account::YUNFENBA_NAME,
+        ],
+        Account::AQIINFO => [
+            Util::murl('aqiinfo'),
+            Account::    AQIINFO_NAME,
+        ],
+        Account::ZJBAO => [
+            Util::murl('zjbao'),
+            Account::  ZJBAO_NAME,
+        ],
+        Account::MEIPA => [
+            Util::murl('meipa'),
+            Account::  MEIPA_NAME,
+        ],
+        Account::KINGFANS => [
+            Util::murl('kingfans'),
+            Account::KINGFANS_NAME,
+        ],
+        Account::SNTO => [
+            Util::murl('snto'),
+            Account:: SNTO_NAME,
+        ],
+        Account::YFB => [
+            Util::murl('yfb'),
+            Account::YFB_NAME,
+        ]
+    ];
+
+    foreach ($spec as $type => $item) {
+        $acc = Account::findOneFromUID(Account::makeThirdPartyPlatformUID($type, $item[1]));
+        if ($acc) {
+            $acc->setType($type);
+            $acc->setUrl($item[0]);
+            $acc->save();
+        }
+    }
+
+    updateSettings('accounts.lastupdate', time());
 }

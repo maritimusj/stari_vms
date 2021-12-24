e0a9ac30468dc83f790b75b55c2d398e0bc5c1fe
js <7990434@qq.com>
2021-12-24 11:28:41 +0800

    chore: *

--- inc/web/settings.inc.php
+++ inc/web/settings.inc.php
@@ -252,12 +252,7 @@ if (isset(\$_SERVER['HTTP_STA_API']) || isset(\$_SERVER['HTTP_LLT_API'])) {
         Config::device('door.enable', request::bool('DeviceWithDoor') ? 1 : 0, true);
 
         $settings['app']['first']['enabled'] = request::bool('ZovyeAppFirstEnable') ? 1 : 0;
-        $settings['device']['upload'] = [
-            'url' => request::trim('deviceUploadApiUrl'),
-            'key' => request::trim('deviceUploadAppKey'),
-            'secret' => request::trim('deviceUploadAppSecret'),
-        ];
-        
+
         $settings['app']['domain']['enabled'] = request::bool('MultiDomainEnable') ? 1 : 0;
         $settings['app']['domain']['main'] = trim(request::trim('mainUrl'), '\\\/');
         $settings['app']['domain']['bak'] = request::array('bakUrl');
@@ -645,6 +640,12 @@ if (isset(\$_SERVER['HTTP_STA_API']) || isset(\$_SERVER['HTTP_LLT_API'])) {
 
         $settings['api'] = [
             'account' => request::trim('account'),
+        ];
+
+        $settings['device']['upload'] = [
+            'url' => request::trim('deviceUploadApiUrl'),
+            'key' => request::trim('deviceUploadAppKey'),
+            'secret' => request::trim('deviceUploadAppSecret'),
         ];
 
         if (App::isDonatePayEnabled()) {

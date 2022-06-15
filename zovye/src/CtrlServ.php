1adc9e4cc80e5b74370bd4c9f842652e62ab9cc7
js <7990434@qq.com>
2022-06-15 15:37:52 +0800

    fixed: *

--- zovye/src/CtrlServ.php
+++ zovye/src/CtrlServ.php
@@ -124,6 +124,7 @@ class CtrlServ
             $ctrlServerUrl .= "/$path";
 
             $params['nostr'] = TIMESTAMP;
+            $ctrlServerUrl .= '?'.http_build_query($params);
 
             $headers = [];
 
@@ -170,7 +171,6 @@ class CtrlServ
                 );
             }
 
-            $ctrlServerUrl .= '?'.http_build_query($params);
             return self::$http_client->request($ctrlServerUrl, $method, $headers, $body);
         }
 

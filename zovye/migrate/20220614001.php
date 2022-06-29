2c0987cf4e46b1808005598a257cb12db9511cbd
js <7990434@qq.com>
2022-06-29 11:18:41 +0800

    fixed: POINT字段

--- zovye/migrate/20220614001.php
+++ zovye/migrate/20220614001.php
@@ -8,6 +8,7 @@ if (!We7::pdo_fieldexists($tb_name.'_device_groups', 'extra')) {
     $sql = <<<SQL
     ALTER TABLE `ims_zovye_vms_device_groups` ADD `type_id` INT NOT NULL DEFAULT '0' AFTER `uniacid`, ADD INDEX (`type_id`);
     ALTER TABLE `ims_zovye_vms_device_groups` ADD `extra` JSON NOT NULL AFTER `agent_id`;
+    ALTER TABLE `ims_zovye_vms_device_groups` ADD `loc` POINT NULL AFTER `agent_id`;
 SQL;
     Migrate::execSQL($sql);
 }

467fc362b3ea7c373b18264f523fa2a5917ab277
js <7990434@qq.com>
2023-01-10 13:28:05 +0800

    fixed: *

--- inc/web/order.inc.php
+++ inc/web/order.inc.php
@@ -394,6 +394,28 @@ if ($op == 'default') {
     );
 
     JSON::success(['title' => '出货记录', 'content' => $content]);
+
+} elseif ($op == 'stopFueling') {
+    $id = request::int('id');
+
+    $order = Order::get($id);
+    if (empty($order)) {
+        JSON::fail('找不到这个订单！');
+    }
+
+    if ($order->isFuelingFinished()) {
+        JSON::fail('这个订单已经结束！');
+    }
+
+    $device = $order->getDevice();
+    if (!$device) {
+        JSON::fail('找不到这个设备！');
+    }
+
+    Fueling::checkUnfinishedOrder($device, "force");
+
+    JSON::success('已强制停止关联订单，请刷新页面检查订单情况！');
+
 } elseif ($op == 'commissionDetail') {
 
     $id = request::int('id');

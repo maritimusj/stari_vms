656614c3c6eda2782982d4791653bfd80f4b842a
jjs <7990434@qq.com>
2023-01-10 15:21:38 +0800

    chore: 支付订单失败退款

--- zovye/src/Order.php
+++ zovye/src/Order.php
@@ -139,7 +139,7 @@ class Order extends State
 
             //退款
             $res = Pay::refund($order_no, $total);
-            $res['total'] = $total;
+            $res['total'] = $total > 0 ? $total : $pay_log->getPrice();
 
             $pay_log->setData(is_error($res) ? 'refund_fail' : 'refund', $res);
             if (!$pay_log->save()) {

82c0bf11da8a3b79109642028a80a7e9e4033688
js <7990434@qq.com>
2023-02-25 15:51:10 +0800

    chore: *

--- inc/web/goods.inc.php
+++ inc/web/goods.inc.php
@@ -441,4 +441,28 @@ if ($op == 'default' || $op == 'goods') {
     );
 
     JSON::success(['z' => date('z'), 'content' => $content]);
+
+} elseif ($op == 'toggle') {
+
+    $goods = Goods::get(request::int('id'));
+    if (empty($goods)) {
+        JSON::fail('找不到这个商品！');
+    }
+
+    $s1 = $goods->getS1();
+
+    switch(request::trim('w')) {
+        case 'free': $s1 = Goods::setFreeBitMask($s1, !$goods->allowFree());break;
+        case 'pay': $s1 = Goods::setPayBitMask($s1, !$goods->allowPay());break;
+        case 'balance': $s1 = Goods::setBalanceBitMask($s1, !$goods->allowBalance());break;
+        case 'delivery': $s1 = Goods::setDeliveryBitMask($s1, !$goods->allowDelivery());break;
+    }
+
+    $goods->setS1($s1);
+
+    if ($goods->save()) {
+        JSON::success('操作成功！');
+    }
+
+    JSON::fail('操作失败！');
 }

33ed43fffba1fee694370527ccc171fba4a1c6f7
jjs <7990434@qq.com>
2023-02-28 13:43:55 +0800

    chore: *

--- zovye/include/global.php
+++ zovye/include/global.php
@@ -6,32 +6,39 @@
 
 namespace zovye;
 
-//捕获错误和异常
 use Exception;
 
+//定义常量REQUEST_ID
 define('REQUEST_ID', Util::generateUID());
 
+//捕获错误和异常
 Util::setErrorHandler();
 
-request::setData($GLOBALS['_GPC']);
-
-//初始化事件驱动
-EventBus::init();
-
-//设置httpClient
-$http_client = new we7HttpClient();
-
-CtrlServ::setHttpClient($http_client);
-ChargingServ::setHttpClient($http_client);
-
-We7::load()->func('cache');
-
-//启动应用
 try {
+    //设置request数据来源
+    request::setData($GLOBALS['_GPC']);
+
+    //初始化事件驱动
+    EventBus::init();
+
+    //设置httpClient
+    CtrlServ::setHttpClient(new we7HttpClient());
+
+    if (App::isChargingDeviceEnabled()) {
+        ChargingServ::setHttpClient(new we7HttpClient());
+    }
+
+    //默认加载缓存
+    We7::load()->func('cache');
+
+    //启动应用
     app()->run();
+
 } catch (Exception $e) {
+
     Log::error("app", [
         'error' => $e->getMessage(),
         'trace' => $e->getTraceAsString(),
     ]);
+
 }

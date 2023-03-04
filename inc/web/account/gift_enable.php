3c7cd56244b29baafafdfa350063c4fadce0e208
js <7990434@qq.com>
2023-03-04 12:28:13 +0800

    chore: *

---
+++ inc/web/account/gift_enable.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+
+namespace zovye;
+
+defined('IN_IA') or exit('Access Denied');
+
+$id = request::int('id');
+if ($id > 0) {
+    $gift = FlashEgg::getGift($id);
+    if ($gift) {
+        $enabled = $gift->isEnabled();
+        $gift->setEnabled(!$enabled);
+        if ($gift->save()) {
+            JSON::success([
+                'enabled' => $gift->isEnabled(),
+            ]);
+        }
+    }
+}
+JSON::fail('找不到这个活动！');
+
+
+

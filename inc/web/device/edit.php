70431474be1d9107cb53635c22b10c08cb4d020d
js <7990434@qq.com>
2023-03-09 16:54:00 +0800

    fixed: *

--- inc/web/device/edit.php
+++ inc/web/device/edit.php
@@ -12,7 +12,6 @@ use zovye\account\MoscaleAccount;
 use zovye\account\ZhiJinBaoAccount;
 use zovye\model\device_groupsModelObj;
 
-$type = Request::str('type');
 $id = Request::int('id');
 $device_types = [];
 
@@ -41,26 +40,21 @@ if ($id) {
         $tpl_data['bluetooth'] = $device->settings('extra.bluetooth', []);
     }
 
-    $agent = $device->getAgent();
+    $model = $device->getDeviceModel();
 
+    $agent = $device->getAgent();
     $tpl_data['agent'] = $agent;
+
 } else {
-    if ($type == 'vd') {
+
+    $model = Request::str('model');
+    if ($model == 'vd') {
         $tpl_data['vd_imei'] = 'V'.Util::random(15, true);
     }
+
 }
 
-if ($type == 'vd' || (isset($device) && $device->isVDevice())) {
-    $tpl_data['device_model'] = Device::VIRTUAL_DEVICE;
-} elseif ($type == 'bluetooth_device' || (isset($device) && $device->isBlueToothDevice())) {
-    $tpl_data['device_model'] = Device::BLUETOOTH_DEVICE;
-} elseif ($type == 'charging' || (isset($device) && $device->isChargingDevice())) {
-    $tpl_data['device_model'] = Device::CHARGING_DEVICE;
-}  elseif ($type == 'fueling' || (isset($device) && $device->isFuelingDevice())) {
-    $tpl_data['device_model'] = Device::FUELING_DEVICE;
-} else {
-    $tpl_data['device_model'] = Device::NORMAL_DEVICE;
-}
+$tpl_data['device_model'] = $model;
 
 $tpl_data['bluetooth']['protocols'] = BlueToothProtocol::all();
 $tpl_data['device_types'] = $device_types;
@@ -82,19 +76,19 @@ if (isset($device) && App::isZJBaoEnabled() && ZhiJinBaoAccount::isAssigned($dev
 }
 
 $module_url = MODULE_URL;
-if ($type == 'vd' || (isset($device) && $device->isVDevice())) {
+if ($model == Device::VIRTUAL_DEVICE) {
     $icon_html = <<<HTML
     <img src="{$module_url}static/img/vdevice.svg" class="icon" title="虚拟设备">
 HTML;
-} elseif ($type == 'bluetooth_device' || (isset($device) && $device->isBlueToothDevice())) {
+} elseif ($model == Device::BLUETOOTH_DEVICE) {
     $icon_html = <<<HTML
     <img src="{$module_url}static/img/bluetooth.svg" class="icon" title="蓝牙设备">
 HTML;
-} elseif ($type == 'charging' || (isset($device) && $device->isChargingDevice())) {
+} elseif ($model == Device::CHARGING_DEVICE) {
     $icon_html = <<<HTML
     <img src="{$module_url}static/img/charging.svg" class="icon" title="充电桩">
 HTML;
-} elseif ($type == 'fueling' || (isset($device) && $device->isFuelingDevice())) {
+} elseif ($model == Device::FUELING_DEVICE) {
     $icon_html = <<<HTML
     <img src="{$module_url}static/img/fueling.svg" class="icon" title="尿素加注设备">
 HTML;
@@ -105,7 +99,7 @@ HTML;
 }
 
 $groups = [];
-if ($type == 'charging' || (isset($device) && $device->isChargingDevice())) {
+if ($model == Device::CHARGING_DEVICE) {
     $group_query = Group::query(Group::CHARGING);
 } else {
     $group_query = Group::query(Group::NORMAL);
@@ -119,11 +113,12 @@ foreach ($group_query->findAll() as $val) {
 $tpl_data['groups'] = $groups;
 $tpl_data['icon'] = $icon_html;
 $tpl_data['from'] = Request::str('from', 'base');
-$tpl_data['is_normal_device'] = $type == '' || (isset($device) && $device->isNormalDevice());
-$tpl_data['is_bluetooth_device'] = $type == 'bluetooth_device' || (isset($device) && $device->isBlueToothDevice());
-$tpl_data['is_vdevice'] = $type == 'vd' || (isset($device) && $device->isVDevice());
-$tpl_data['is_charging_device'] = $type == 'charging' || (isset($device) && $device->isChargingDevice());
-$tpl_data['is_fueling_device'] = $type == 'fueling' || (isset($device) && $device->isFuelingDevice());
 $tpl_data['themes'] = Theme::all();
+
+$tpl_data['is_normal_device'] = $model == Device::NORMAL_DEVICE;
+$tpl_data['is_bluetooth_device'] = $model == Device::BLUETOOTH_DEVICE;
+$tpl_data['is_vdevice'] = $model == Device::VIRTUAL_DEVICE;
+$tpl_data['is_charging_device'] = $model == Device::CHARGING_DEVICE;
+$tpl_data['is_fueling_device'] = $model == Device::FUELING_DEVICE;
 
 app()->showTemplate('web/device/edit_new', $tpl_data);

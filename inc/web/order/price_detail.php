216b92265bb1d5a19bce54ac4ce1acc62102343b
jjs <7990434@qq.com>
2023-04-10 09:48:18 +0800

    fixed: 加注订单结算详情

--- inc/web/order/price_detail.php
+++ inc/web/order/price_detail.php
@@ -6,6 +6,10 @@
  
 namespace zovye;
 
+use DateInterval;
+use DateTime;
+use Exception;
+
 defined('IN_IA') or exit('Access Denied');
 
 $id = Request::int('id');
@@ -15,11 +19,41 @@ if (!$order) {
     JSON::fail('找不到这个订单！');
 }
 
-$content = app()->fetchTemplate(
-    'web/charging/detail',
-    [
-        'data' => $order->getChargingRecord(),
-    ]
-);
+if ($order->isFuelingOrder()) {
+    $data = $order->getFuelingRecord();
+    if ($data['time_total'] > 0) {
+        try {
+            $interval = new DateInterval('PT'.$data['time_total'].'S');
+            $time = new DateTime('00:00:00');
+            $time->add($interval);
+
+            if ($data['time_total'] < 60) {
+                $format_str = 's';
+            } elseif ($data['time_total'] < 60 * 60) {
+                $format_str = 'i:s';
+            } else {
+                $format_str = 'H:i:s';
+            }
+            $data['time_total_formatted'] = $time->format($format_str);
+        } catch (Exception $e) {
+        }
+    }
+
+    $content = app()->fetchTemplate(
+        'web/fueling/detail',
+        [
+            'data' => $data,
+        ]
+    );
+} elseif ($order->isChargingOrder()) {
+    $content = app()->fetchTemplate(
+        'web/charging/detail',
+        [
+            'data' => $order->getChargingRecord(),
+        ]
+    );
+} else {
+    $content = '没有计费信息!';
+}
 
 JSON::success(['title' => '计费详情', 'content' => $content]);

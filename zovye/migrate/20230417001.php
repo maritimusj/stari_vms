611db1fb62a18f1010a61363d62f99bea5ba394f
jjs <7990434@qq.com>
2023-04-17 10:44:38 +0800

    chore: 整理身份相关代码

--- zovye/migrate/20230417001.php
+++ zovye/migrate/20230417001.php
@@ -2,6 +2,8 @@
 
 namespace zovye;
 
+use zovye\model\agentModelObj;
+
 $tb_name = APP_NAME;
 
 if (We7::pdo_fieldexists($tb_name.'_user', 'passport')) {
@@ -16,4 +18,14 @@ if (We7::pdo_fieldexists($tb_name.'_principal', 'enable')) {
 ALTER TABLE `ims_zovye_vms_principal` CHANGE `enable` `enabled` TINYINT(4) NOT NULL DEFAULT '1';
 SQL;
     Migrate::execSQL($sql);
+}
+
+/** @var agentModelObj $agent */
+foreach (Agent::query()->findAll() as $agent) {
+    if (empty($agent->getAgentLevel())) {
+        $level = $agent->getAgentData('level');
+        if ($level) {
+            $agent->setAgent($level);
+        }
+    }
 }

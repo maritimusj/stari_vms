ba14e8412626e2b562462782ed5573b68be21496
jjs <7990434@qq.com>
2023-04-17 15:52:16 +0800

    chore: referral

--- inc/mobile/promoter.inc.php
+++ inc/mobile/promoter.inc.php
@@ -6,9 +6,31 @@
 
 namespace zovye;
 
+use zovye\api\wx\balance;
+
+if (!App::isPromoterEnabled()) {
+    Util::resultAlert('这个功能没有启用，谢谢！', 'error');
+}
+
 $user = Util::getCurrentUser();
 if (empty($user)) {
     Util::resultAlert('请在微信中打开，谢谢！', 'error');
+}
+
+if ($user->isBanned()) {
+    Util::resultAlert('对不起，暂时无法使用这个功能！', 'error');
+}
+
+if ($user->isAgent()) {
+    Util::resultAlert('对不起，代理商无法使用这个功能！', 'error');
+}
+
+if ($user->isPartner()) {
+    Util::resultAlert('对不起，合伙人无法使用这个功能！', 'error');
+}
+
+if ($user->isKeeper()) {
+    Util::resultAlert('对不起，运营人员无法使用这个功能！', 'error');
 }
 
 $op = Request::op('default');
@@ -68,23 +90,29 @@ JSCODE;
 
     $code = Request::trim('code');
     if (empty($code)) {
-        JSON::fail('输入的推荐码无效，请重新输入！');
+        JSON::fail('输入的邀请码无效，请重新输入！');
     }
 
     $ref = Referral::from($code);
     if (empty($ref)) {
-        JSON::fail('输入的推荐码无效，请重新输入！');
+        JSON::fail('输入的邀请码无效，请重新输入！');
     }
 
     $keeper_user = $ref->getUser();
     if (empty($keeper_user) || $keeper_user->isBanned()) {
-        JSON::fail('找不到推荐码对应的运营人员！');
+        JSON::fail('找不到邀请码对应的运营人员！');
+    }
+
+    if ($keeper_user->getId() == $user->getId()) {
+        JSON::fail('不能邀请自己，谢谢！');
     }
 
     $keeper = $keeper_user->getKeeper();
     if (empty($keeper)) {
         JSON::fail('找不到推荐码对应的运营人员！');
     }
+
+    $user->setSuperiorId($keeper->getId());
 
     if ($user->setPrincipal(Principal::Promoter, [
         'keeper' => $keeper->getId(),
@@ -118,4 +146,45 @@ JSCODE;
     }
 
     JSON::success($result);
+
+} elseif ($op == 'pre_withdraw') {
+    $tpl_data = [];
+
+    $api_url = Util::murl('promoter');
+    $jquery_url = JS_JQUERY_URL;
+    $vueJs_url = JS_VUE_URL;
+    $js_sdk = Util::fetchJSSDK();
+
+    $tpl_data['js']['code'] = <<<JSCODE
+    <script src="$jquery_url"></script>
+    <script src="$vueJs_url"></script>
+    $js_sdk
+    <script>
+        wx.ready(function(){
+            wx.hideAllNonBaseMenuItem();
+            wx.showMenuItems({
+                menuList: [
+                    "menuItem:share:appMessage",
+                    "menuItem:share:timeline",
+                    "menuItem:favorite",
+                    "menuItem:copyUrl",
+                ] // 要显示的菜单项，所有menu项见附录3
+            });
+        });
+        const zovye_fn = {
+            api_url: "$api_url",
+        }
+        zovye_fn.brief = function() {
+            return $.getJSON('$api_url', {op: 'brief'});
+        };
+        zovye_fn.withdraw = function(amount) {
+            return $.getJSON('$api_url', {op: 'withdraw', amount});
+        };
+</script>
+JSCODE;
+
+    app()->showTemplate('promoter/withdraw', ['tpl' => $tpl_data]);
+
+} elseif ($op == 'withdraw') {
+    return balance::balanceWithdraw($user, Request::float('amount', 0, 2) * 100);
 }

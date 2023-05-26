f86ab642503f26b5a7dac81585569611270f9c5d
jjs <7990434@qq.com>
2023-05-26 11:30:36 +0800

    chore: *

--- lib/bluetooth/lingdong/OpenDeviceCmd.php
+++ lib/bluetooth/lingdong/OpenDeviceCmd.php
@@ -9,8 +9,16 @@ namespace bluetooth\lingdong;
 
 class OpenDeviceCmd extends cmd
 {
+    private $locker_id;
+
     public function __construct($device_id, $locker_id)
     {
+        $this->locker_id = $locker_id;
         parent::__construct($device_id, 0x02, [$locker_id, 0x0, 0x0, 0x0, 0x0]);
+    }
+
+    function getMessage(): string
+    {
+        return '<= 请求开锁, '.$this->locker_id;
     }
 }

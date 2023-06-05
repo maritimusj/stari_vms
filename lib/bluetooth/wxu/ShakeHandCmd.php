d1a3dff3bb7cf624cf4df951f9b85cfa2f619b3c
jjs <7990434@qq.com>
2023-06-05 09:31:35 +0800

    fixed: 蓝牙协议wxu1.1

--- lib/bluetooth/wxu/ShakeHandCmd.php
+++ lib/bluetooth/wxu/ShakeHandCmd.php
@@ -13,6 +13,6 @@ class ShakeHandCmd extends cmd
     public function __construct($device_id)
     {
         Helper::resetSEQ($device_id);
-        parent::__construct($device_id, 0x01, [0x5C, 0x5F, 0x57, 0x59, 0x5F]);
+        parent::__construct($device_id, 0x01, [0x5C, 0x5F, 0x57, 0x59, 0x5E]);
     }
 }

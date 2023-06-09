72dd01d51225986c291166b3a4335a72d6667dc9
js <7990434@qq.com>
2023-06-09 10:23:12 +0800

    chore: *

--- inc/web/qr.inc.php
+++ inc/web/qr.inc.php
@@ -3,12 +3,29 @@ namespace zovye;
 
 use ZipArchive;
 
-$url_tmpl = "https://xxx.com/app/index.php?i=2&c=entry&app=NULL&from=device&device={uid}&do=entry&m=zy_saas";
+$url_tmpl = "https://saas.jspercent.com/app/index.php?i=2&c=entry&app=NULL&from=device&device={uid}&do=entry&m=zy_saas";
 
-$list = [];
+$list = [
+"863650067311587",
+"863650067312874",
+"863650067312890",
+"863650067313245",
+"863650067311066",
+"863650067311496",
+"862408069636266",
+"863650067310969",
+"863650067536589",
+"863650067536159",
+"863650067311108",
+"863650067537355",
+"863650067303378",
+"863650067303519",
+"863650067312080",
+];
 
 $zip = new ZipArchive();
-$zip_filename = ATTACHMENT_ROOT . date('YmdHIs').'.zip';
+$filename =  date('YmdHIs').'.zip';
+$zip_filename = ATTACHMENT_ROOT . $filename;
 $zip->open($zip_filename, ZipArchive::CREATE);
 
 foreach($list as $uid) {
@@ -20,4 +37,6 @@ foreach($list as $uid) {
 }
 
 $zip->close();
+
+Util::redirect(Util::toMedia($filename));
 

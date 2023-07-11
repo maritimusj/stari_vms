efcf59edec262609dabafa24df98d4de45ae0d3b
jjs <7990434@qq.com>
2023-07-11 15:17:05 +0800

    chore: 记录退款金额

--- inc/web/order/refund_detail.php
+++ inc/web/order/refund_detail.php
@@ -11,6 +11,25 @@ defined('IN_IA') or exit('Access Denied');
 $id = Request::int('id');
 
 $order = Order::get($id);
+
 if (empty($order)) {
-    JSON::fail('找不到这个上订单！');
+    JSON::fail('找不到这个订单！');
 }
+
+if (!$order->isRefund()) {
+    JSON::fail('订单没有退款！');
+}
+
+$log = Pay::getPayLog($order->getOrderNO());
+
+if (empty($log)) {
+    JSON::fail('找不到支付记录！');
+}
+
+$refund = $log->getData('refund', []);
+
+if (empty($refund)) {
+    JSON::fail('没有退款数据！');
+}
+
+

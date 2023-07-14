e3f6525243339449f3dc35b78c2d224f5aad91ea
jjs <7990434@qq.com>
2023-07-14 09:09:06 +0800

    chore: *

--- inc/web/qr.inc.php
+++ inc/web/qr.inc.php
@@ -3,7 +3,7 @@ namespace zovye;
 
 use ZipArchive;
 
-$url_tmpl = "https://xxx.com/app/index.php?i=2&c=entry&app=NULL&from=device&device={uid}&do=entry&m=zy_saas";
+$template_url = "https://xxx.com/app/index.php?i=2&c=entry&app=NULL&from=device&device={uid}&do=entry&m=zy_saas";
 
 $list = [
     //加入需要创建二维码的网址
@@ -15,7 +15,7 @@ $zip_filename = ATTACHMENT_ROOT . $filename;
 $zip->open($zip_filename, ZipArchive::CREATE);
 
 foreach($list as $uid) {
-    $url = str_replace("{uid}", $uid, $url_tmpl);
+    $url = str_replace('{uid}', $uid, $template_url);
     QRCodeUtil::createFile("device.$uid", $url, function ($filename) use($uid, $zip) {
         QRCodeUtil::renderTxt($filename, $uid);
         $zip->addFile($filename, "$uid.png");

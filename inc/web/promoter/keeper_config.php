2d094358d14c8b76c856a7a8129e0686e72fa380
js <7990434@qq.com>
2023-08-02 13:04:33 +0800

    chore: *

---
+++ inc/web/promoter/keeper_config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+
+namespace zovye;
+
+defined('IN_IA') or exit('Access Denied');
+
+$keeper_id = Request::int('id');
+
+$keeper = Keeper::get($keeper_id);
+if (empty($keeper)) {
+    JSON::fail('找不到这个运营人员！');
+}
+
+$config = $keeper->settings('notice', []);
+
+Response::templateJSON(
+    'web/keeper/config',
+    '配置',
+    [
+        'id' => $keeper->getId(),
+        'config' => $config,
+    ]
+);
---
+++ inc/web/promoter/save_keeper_config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+ 
+namespace zovye;
+
+defined('IN_IA') or exit('Access Denied');
+
+$keeper_id = Request::int('id');
+
+$keeper = Keeper::get($keeper_id);
+if (empty($keeper)) {
+    JSON::fail('找不到这个运营人员！');
+}
+
+$keeper->updateSettings('notice', [
+    'order' => [
+        'succeed' => Request::bool('orderSucceed') ? 1 : 0,
+        'failed' => Request::bool('orderFailed') ? 1 : 0,
+    ],
+    'device' => [
+        'online' => Request::bool('deviceOnline') ? 1 : 0,
+        'offline' => Request::bool('deviceOffline') ? 1 : 0,
+        'error' => Request::bool('deviceError') ? 1 : 0,
+        'low_battery' => Request::bool('deviceLowBattery') ? 1 : 0,
+        'low_remain' => Request::bool('deviceLowRemain') ? 1 : 0,
+    ]
+]);
+
+JSON::success([
+    'msg' => '保存成功！',
+]);

55ef9765d4e51482df99934c50d2aaa05ed4ad36
js <7990434@qq.com>
2023-08-03 08:18:04 +0800

    fixed: task_vwModelObj

--- zovye/src/model/task_vwModelObj.php
+++ zovye/src/model/task_vwModelObj.php
@@ -3,42 +3,85 @@
  * @author jin@stariture.com
  * @url www.stariture.com
  */
+
 namespace zovye\model;
 
+use zovye\Account;
 use zovye\base\modelObj;
 use zovye\traits\ExtraDataGettersAndSetters;
+use zovye\User;
+
 use function zovye\tb;
 
+/**
+ * @method getAccountId()
+ * @method getUserId()
+ * @method getS1()
+ * @method getState()
+ * @method setS1(int $INIT)
+ */
 class task_vwModelObj extends modelObj
 {
     public static function getTableName($readOrWrite): string
     {
         return tb('task_vw');
     }
-	/** @var int */
-	protected $id;
 
-	/** @var int */
-	protected $user_id;
+    /** @var int */
+    protected $id;
 
-	/** @var int */
-	protected $account_id;
+    /** @var int */
+    protected $user_id;
 
-	/** @var int */
-	protected $s1;
+    /** @var int */
+    protected $account_id;
 
-	/** @var varchar */
-	protected $s2;
+    /** @var int */
+    protected $s1;
 
-	/** @var text */
-	protected $extra;
+    /** @var string */
+    protected $s2;
 
-	/** @var int */
-	protected $createtime;
+    /** @var int */
+    protected $state;
 
-	/** @var smallint */
-	protected $state;
+    protected $extra;
 
+    /** @var int */
+    protected $createtime;
 
-	use ExtraDataGettersAndSetters;
+    private $account_obj = null;
+
+    use ExtraDataGettersAndSetters;
+
+    public function getAccount(): ?accountModelObj
+    {
+        if (!isset($this->account_obj)) {
+            $this->account_obj = Account::get($this->account_id);
+        }
+
+        return $this->account_obj;
+    }
+
+    public function getUser(): ?userModelObj
+    {
+        return User::get($this->user_id);
+    }
+
+    public function getUid(): string
+    {
+        return $this->s2;
+    }
+
+    public function format(): array
+    {
+        $acc = $this->getAccount();
+        $result = $acc ? $acc->format() : [];
+        if ($result) {
+            $result['status'] = $this->s1;
+            $result['createtime'] = $this->createtime;
+        }
+
+        return $result;
+    }
 }

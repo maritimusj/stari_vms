37c98a1823304c7c1fcc85ee6ed2b3950bd24fb6
jjs <7990434@qq.com>
2023-08-26 07:47:52 +0800

    chore: *

--- zovye/include/global.php
+++ zovye/include/global.php
@@ -12,30 +12,6 @@ use Exception;
 define('REQUEST_ID', Util::generateUID());
 
 try {
-    //默认加载缓存
-    We7::load()->func('cache');
-
-    Request::extraAjaxJsonData();
-
-    //设置request数据源
-    Request::setData($GLOBALS['_GPC']);
-
-    //初始化日志
-    Log::init(new FileLogWriter(), settings('app.log.level', LOG_LEVEL));
-
-    //捕获错误和异常
-    Util::setErrorHandler();
-
-    //初始化事件驱动
-    EventBus::init();
-
-    //设置CtrlServ
-    CtrlServ::init(new we7HttpClient(), settings('ctrl', []));
-
-    //初始化充电设备服务
-    if (App::isChargingDeviceEnabled()) {
-        ChargingServ::setHttpClient(new we7HttpClient(), Config::charging('server', []));
-    }
 
     //启动应用
     app()->run();
--- zovye/src/WeApp.php
+++ zovye/src/WeApp.php
@@ -125,6 +125,31 @@ class WeApp extends Settings
      */
     public function run(): WeApp
     {
+        //默认加载缓存
+        We7::load()->func('cache');
+
+        Request::extraAjaxJsonData();
+
+        //设置request数据源
+        Request::setData($GLOBALS['_GPC']);
+
+        //初始化日志
+        Log::init(new FileLogWriter(), settings('app.log.level', LOG_LEVEL));
+
+        //捕获错误和异常
+        Util::setErrorHandler();
+
+        //初始化事件驱动
+        EventBus::init();
+
+        //设置CtrlServ
+        CtrlServ::init(new we7HttpClient(), settings('ctrl', []));
+
+        //初始化充电设备服务
+        if (App::isChargingDeviceEnabled()) {
+            ChargingServ::setHttpClient(new we7HttpClient(), Config::charging('server', []));
+        }
+
         class_alias(__NAMESPACE__.'\Site', lcfirst(APP_NAME).'ModuleSite');
 
         return $this;

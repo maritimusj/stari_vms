5ff7a0b29e10f867aa43a8a70312b564eebbfe82
jjs <7990434@qq.com>
2023-08-26 15:48:38 +0800

    chore: *

--- zovye/migrate/20230822001.php
+++ zovye/migrate/20230822001.php
@@ -8,21 +8,21 @@ $tb_name = APP_NAME;
 
 if (!We7::pdo_field_exists($tb_name.'_order', 'transaction_id')) {
     $sql = <<<SQL
-ALTER TABLE `ims_zovye_vms_order` ADD `transaction_id` VARCHAR(32) NULL AFTER `order_id`, ADD INDEX (`uniacid`, `transaction_id`);
+ALTER TABLE `ims_zovye_vms_order` ADD `transaction_id` VARCHAR(32) NULL AFTER `order_id`, ADD INDEX t1 (`uniacid`, `transaction_id`);
 SQL;
     Migrate::execSQL($sql);
 }
 
 if (!We7::pdo_field_exists($tb_name.'_order', 'user_id')) {
     $sql = <<<SQL
-ALTER TABLE `ims_zovye_vms_order` ADD `user_id` VARCHAR(32) NULL AFTER `goods_id`, ADD INDEX (`uniacid`, `user_id`);
+ALTER TABLE `ims_zovye_vms_order` ADD `user_id` VARCHAR(32) NULL AFTER `goods_id`, ADD INDEX u1 (`uniacid`, `user_id`);
 SQL;
     Migrate::execSQL($sql);
 }
 
 if (!We7::pdo_field_exists($tb_name.'_order', 'account_id')) {
     $sql = <<<SQL
-ALTER TABLE `ims_zovye_vms_order` ADD `account_id` VARCHAR(32) NULL AFTER `goods_id`, ADD INDEX (`uniacid`, `account_id`);
+ALTER TABLE `ims_zovye_vms_order` ADD `account_id` VARCHAR(32) NULL AFTER `goods_id`, ADD INDEX a1 (`uniacid`, `account_id`);
 SQL;
     Migrate::execSQL($sql);
 }

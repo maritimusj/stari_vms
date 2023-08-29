69aae016a946e1043f9dc991624015db6e384fb1
js <7990434@qq.com>
2023-08-29 08:51:12 +0800

    fixed: 批量删除设备

--- inc/web/device/remove.php
+++ inc/web/device/remove.php
@@ -6,55 +6,72 @@
 
 namespace zovye;
 
+use Exception;
+use RuntimeException;
+
 defined('IN_IA') or exit('Access Denied');
 
-$device = Device::get(Request::int('id'));
-if (empty($device)) {
-    Response::toast('删除失败！', Util::url('device'), 'error');
-}
-
-We7::load()->func('file');
-We7::file_remote_delete($device->getQrcode());
-
-$device->remove('assigned');
-$device->remove('adsData');
-$device->remove('accountsData');
-$device->remove('lastErrorNotify');
-$device->remove('lastRemainWarning');
-$device->remove('fakeQrcodeData');
-$device->remove('lastApkUpdate');
-$device->remove('firstMsgStatistic');
-$device->remove('location');
-$device->remove('statsData');
-$device->remove('lastErrorData');
-$device->remove('extra');
-
-if ($device->isChargingDevice()) {
-    ChargingNowData::removeAllByDevice($device);
-}
-
-//删除相关套餐
-foreach (Package::query(['device_id' => $device->getId()])->findAll() as $entry) {
-    $entry->destroy();
-}
-
-if ($device->isNormalDevice()) {
-    $imei = $device->getImei();
-    if (!empty($imei)) {
-        $res = Device::release($imei);
-        if (is_error($res)) {
-            Log::error('device', [
-                'imei' => $imei,
-                'message' => '释放设备失败！',
-                'result' => $res,
-            ]);
+try {
+    $device = Device::get(Request::int('id'));
+    if (empty($device)) {
+        throw new RuntimeException('找不到这个设备！');
+    }
+    
+    We7::load()->func('file');
+    We7::file_remote_delete($device->getQrcode());
+    
+    $device->remove('assigned');
+    $device->remove('adsData');
+    $device->remove('accountsData');
+    $device->remove('lastErrorNotify');
+    $device->remove('lastRemainWarning');
+    $device->remove('fakeQrcodeData');
+    $device->remove('lastApkUpdate');
+    $device->remove('firstMsgStatistic');
+    $device->remove('location');
+    $device->remove('statsData');
+    $device->remove('lastErrorData');
+    $device->remove('extra');
+    
+    if ($device->isChargingDevice()) {
+        ChargingNowData::removeAllByDevice($device);
+    }
+    
+    //删除相关套餐
+    foreach (Package::query(['device_id' => $device->getId()])->findAll() as $entry) {
+        $entry->destroy();
+    }
+    
+    if ($device->isNormalDevice()) {
+        $imei = $device->getImei();
+        if (!empty($imei)) {
+            $res = Device::release($imei);
+            if (is_error($res)) {
+                Log::error('device', [
+                    'imei' => $imei,
+                    'message' => '释放设备失败！',
+                    'result' => $res,
+                ]);
+            }
         }
     }
+    
+    //通知实体设备
+    $device->appNotify('update');
+    
+    $device->destroy();
+    
+    if (Request::is_ajax()) {
+        JSON::success(['msg' => '删除成功！']);
+    }
+    
+    Response::toast('删除成功！', Util::url('device'), 'success');
+
+} catch(Exception $e) {
+    
+    if (Request::is_ajax()) {
+        JSON::fail(['msg' => $e->getMessage()]);
+    } else {
+        Response::toast($e->getMessage(), Util::url('device'), 'error');
+    }
 }
-
-//通知实体设备
-$device->appNotify('update');
-
-$device->destroy();
-
-Response::toast('删除成功！', Util::url('device'), 'success');

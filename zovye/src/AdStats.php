8459a92c86807894a549a90c740004e79adc565e
jjs <7990434@qq.com>
2023-09-08 11:03:03 +0800

    chore: AdStats

---
+++ zovye/src/AdStats.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+
+
+namespace zovye;
+
+class AdStats
+{
+    public static function model(): base\modelFactory
+    {
+        return m('advs_stats');
+    }
+}

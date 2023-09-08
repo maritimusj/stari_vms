70ff1936b835d0527013bdd5bcff817d51cab27c
js <7990434@qq.com>
2023-09-08 10:38:32 +0800

    chore: *

---
+++ zovye/src/model/goods_expire_alertModelObj.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+namespace zovye\model;
+
+use zovye\base\modelObj;
+use function zovye\tb;
+
+class goods_expire_alertModelObj extends modelObj
+{
+    public static function getTableName($readOrWrite): string
+    {
+        return tb('goods_expire_alert');
+    }
+    
+	/** @var int */
+	protected $id;
+
+	/** @var int */
+	protected $agent_id;
+
+	/** @var int */
+	protected $device_id;
+
+	/** @var int */
+	protected $lane_id;
+
+	/** @var int */
+	protected $goods_id;
+
+	/** @var int */
+	protected $expired_at;
+
+	/** @var text */
+	protected $extra;
+
+	/** @var int */
+	protected $createtime;
+
+	use \zovye\traits\ExtraDataGettersAndSetters;
+}

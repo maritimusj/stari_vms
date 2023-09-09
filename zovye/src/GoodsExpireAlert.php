c071e39d696e35ac7401959a4ff040899c4e164e
jjs <7990434@qq.com>
2023-09-09 14:51:36 +0800

    chore: *

--- zovye/src/GoodsExpireAlert.php
+++ zovye/src/GoodsExpireAlert.php
@@ -10,34 +10,11 @@ namespace zovye;
 use zovye\model\deviceModelObj;
 use zovye\model\goods_expire_alertModelObj;
 
-class GoodsExpireAlert
+class GoodsExpireAlert extends Base
 {
     public static function model(): base\modelFactory
     {
         return m('goods_expire_alert');
-    }
-
-    public static function create($data = []): ?goods_expire_alertModelObj
-    {
-        if (isset($data['extra'])) {
-            $data['extra'] = json_encode($data['extra']);
-        }
-        return self::model()->create($data);
-    }
-
-    public static function query($condition = []): base\modelObjFinder
-    {
-        return self::model()->where($condition);
-    }
-
-    public static function findOne($condition = [])
-    {
-        return self::query($condition)->findOne();
-    }
-
-    public static function delete($condition = []): bool
-    {
-        return self::model()->delete($condition);
     }
 
     public static function getFor(deviceModelObj $device, int $index, $goods_id = 0, $agent_restrict = true): ?goods_expire_alertModelObj

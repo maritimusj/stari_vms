edef837a1c6e519979ce8fca24928dc088d68f81
jjs <7990434@qq.com>
2023-09-09 13:47:53 +0800

    chore: *

---
+++ zovye/src/Base.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @author jin@stariture.com
+ * @url www.stariture.com
+ */
+
+
+namespace zovye;
+
+class Base
+{
+    public static function model(): model\base\modelFactory
+    {
+        trigger_error('Base::model not implemented', E_USER_ERROR);
+    }
+
+    public static function create($data)
+    {
+        if (is_callable($data)) {
+            $data = call_user_func($data);
+        }
+
+        if (!is_array($data)) {
+            trigger_error('data is not an array', E_USER_ERROR);
+        }
+
+        $classname = static::model()->objClassname();
+
+        if (property_exists($classname, 'uniacid')) {
+            $data['uniacid'] = We7::uniacid();
+        }
+
+        if (property_exists($classname, 'extra') && isset($data['extra'])) {
+            $data['extra'] = call_user_func([$classname, 'serializeExtra'], $data['extra']);
+        }
+
+        return static::model()->create($data);
+    }
+
+    public static function query($condition): model\base\modelObjFinder
+    {
+        return self::model()->query($condition);
+    }
+
+    public static function findOne($condition)
+    {
+        return self::model()->findOne($condition);
+    }
+
+    public static function exists($condition): bool
+    {
+        return self::model()->exists($condition);
+    }
+
+    public static function remove($condition): bool
+    {
+        return self::model()->remove($condition);
+    }
+}

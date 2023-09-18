7d9bf2c8f0796f34f9e1e5d2cbee8ef4970cd1d7
jjs <7990434@qq.com>
2023-09-18 17:10:00 +0800

    chore: *

--- zovye/src/api/router.php
+++ zovye/src/api/router.php
@@ -7,6 +7,11 @@
 namespace zovye\api;
 
 use Exception;
+use ReflectionMethod;
+use zovye\api\wx\common;
+use zovye\domain\Agent;
+use zovye\domain\Keeper;
+use zovye\domain\User;
 use zovye\JSON;
 
 class router
@@ -17,8 +22,24 @@ class router
 
         if (is_callable($fn)) {
             try {
-                $result = call_user_func($fn);
+                $args = [];
+
+                $ref = new ReflectionMethod($fn);
+                foreach ($ref->getParameters() as $arg) {
+                    if ($arg instanceof User) {
+                        $args[] = common::getUser();
+                    } elseif ($arg instanceof Agent) {
+                        $args[] = common::getAgent();
+                    } elseif ($arg instanceof Keeper) {
+                        $args[] = common::getKeeper();
+                    } else {
+                        trigger_error("can't resolve args of method", E_USER_ERROR);
+                    }
+                }
+
+                $result = call_user_func_array($fn, $args);
                 JSON::result($result);
+
             } catch (Exception $e) {
                 JSON::fail($e);
             }

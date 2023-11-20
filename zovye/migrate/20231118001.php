3b3804ed9540a8e1a356de25791d2273f383b8df
jjs <7990434@qq.com>
2023-11-20 13:01:47 +0800

    chore: 支付配置

--- zovye/migrate/20231118001.php
+++ zovye/migrate/20231118001.php
@@ -2,6 +2,8 @@
 
 namespace zovye;
 
+use zovye\domain\PaymentConfig;
+
 defined('IN_IA') or exit('Access Denied');
 
 if (!We7::pdo_table_exists(APP_NAME.'_payment_config')) {
@@ -17,4 +19,60 @@ CREATE TABLE `ims_zovye_vms_payment_config` (
     UNIQUE (`uniacid`, `agent_id`, `name`)) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
 SQL;
     Migrate::execSQL($sql);
+
+    // 创建支付配置
+    $config = settings('pay', []);
+
+    if ($config['lcsw'] && $config['lcsw']['enable']) {
+        PaymentConfig::create([
+            'agent_id' => 0,
+            'name' => Pay::LCSW,
+            'extra' => [
+                'wxapp' => $config['lcsw']['wxapp'],
+                'merchant_no' => $config['lcsw']['merchant_no'],
+                'terminal_id' => $config['lcsw']['terminal_id'],
+                'access_token' => $config['lcsw']['access_token'],
+            ],
+        ]);
+    }
+
+    if ($config['SQB'] && $config['SQB']['enable']) {
+        PaymentConfig::create([
+            'agent_id' => 0,
+            'name' => Pay::SQB,
+            'extra' => [
+
+            ],
+        ]);
+    }
+
+    if ($config['wx'] && $config['wx']['enable']) {
+        PaymentConfig::create([
+            'agent_id' => 0,
+            'name' => Pay::WX,
+            'extra' => [
+                'appid' => $config['wx']['wxapp'],
+                'wxappid' => $config['wx']['wxappid'],
+                'mch_id' => $config['wx']['mch_id'],
+                'sub_mch_id' => $config['wx']['sub_mch_id'],
+                'key' => $config['wx']['key'],
+                'pem' => $config['pem'],
+            ],
+        ]);
+
+        if (!isEmptyArray($config['wx']['v3'])) {
+            PaymentConfig::create([
+                'agent_id' => 0,
+                'name' => Pay::WX_V3,
+                'extra' => [
+                    'appid' => $config['wx']['wxapp'],
+                    'wxappid' => $config['wx']['wxappid'],
+                    'mch_id' => $config['wx']['mch_id'],
+                    'sub_mch_id' => $config['wx']['sub_mch_id'],
+                    'key' => $config['wx']['v3']['key'],
+                    'pem' => $config['v3']['pem'],
+                ],
+            ]);
+        }
+    }
 }

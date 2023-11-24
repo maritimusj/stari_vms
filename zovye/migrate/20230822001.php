3ff80cfcb2dc18dd94e2ca5000974088b43701ac
jjs <7990434@qq.com>
2023-11-24 14:14:37 +0800

    fixed: *

--- zovye/migrate/20230822001.php
+++ zovye/migrate/20230822001.php
@@ -15,14 +15,14 @@ SQL;
 
 if (!We7::pdo_field_exists($tb_name.'_order', 'user_id')) {
     $sql = <<<SQL
-ALTER TABLE `ims_zovye_vms_order` ADD `user_id` VARCHAR(32) NULL AFTER `goods_id`, ADD INDEX u1 (`uniacid`, `user_id`);
+ALTER TABLE `ims_zovye_vms_order` ADD `user_id` INT NULL AFTER `goods_id`, ADD INDEX u1 (`uniacid`, `user_id`);
 SQL;
     Migrate::execSQL($sql);
 }
 
 if (!We7::pdo_field_exists($tb_name.'_order', 'account_id')) {
     $sql = <<<SQL
-ALTER TABLE `ims_zovye_vms_order` ADD `account_id` VARCHAR(32) NULL AFTER `goods_id`, ADD INDEX a1 (`uniacid`, `account_id`);
+ALTER TABLE `ims_zovye_vms_order` ADD `account_id` INT NULL AFTER `goods_id`, ADD INDEX a1 (`uniacid`, `account_id`);
 SQL;
     Migrate::execSQL($sql);
 }

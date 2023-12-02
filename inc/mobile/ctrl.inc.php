cd89c64b049dafe8a9d827b7853c521ecf2e6d58
js <7990434@qq.com>
2023-12-02 17:03:58 +0800

    chore: 兼容原始消息模式

--- inc/mobile/ctrl.inc.php
+++ inc/mobile/ctrl.inc.php
@@ -16,27 +16,53 @@ $data = [];
 if (Request::has('op')) {
     $op = Request::op();
     $data = Request::array('data');
-} elseif (Request::has('extra')) {
+    // 兼容原始设备消息模式
+    if(empty($data['id'])) {
+        $data['id'] = Request::str('id');
+        if (Request::has('version')) {
+            $data['version'] = Request::str('version');
+        }
+    }
+} elseif (Request::has('event')) {
     $op = Request::str('event');
     $data = Request::array('extra');
+    // 兼容原始设备消息模式
+    if (!We7::starts_with($op, 'mcb.')) {
+        $op = 'mcb.'.$op;
+        $data = [
+            'uid' => Request::str('uid'),
+            'extra' => $data,
+        ];
+        if (Request::has('code')) {
+            $data['code'] = Request::str('code');
+        }
+    }
 }
 
 $sign = Request::header('HTTP_ZOVYE_SIGN');
 $no_str = Request::header('HTTP_ZOVYE_NOSTR');
 
+Log::debug('ctrl', function() use ($op, $data) {
+    return [
+        'op' => $op,
+        'data' => $data,
+        'raw' => Request::raw(),
+    ];
+});
+
 $config = settings('ctrl', []);
 
 //检查回调签名
-if ($config['checkSign'] && CtrlServ::makeNotifierSign($config['appKey'], $config['appSecret'],
-        $no_str,
-        Request::raw()
-    ) !== $sign) {
-    Log::fatal('ctrl', [
-        'error' => '签名检验失败！',
-        'op' => $op,
-        'sign' => $sign,
-        'payload' => Request::raw(),
-    ]);
+if ($config['checkSign']) {
+    $x = CtrlServ::makeNotifierSign($config['appKey'], $config['appSecret'],$no_str,Request::raw());
+    if ($x !== $sign) {
+        Log::fatal('ctrl', [
+            'error' => '签名检验失败！',
+            'op' => $op,
+            'sign' => $sign,
+            'raw' => Request::raw(),
+        ]);
+    }
 }
 
 DeviceEventProcessor::handle($op, $data);
